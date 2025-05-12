87b69ab38f1fbd1df7123fb7dc03acc8ab2302ca
Christophe Augello <1112390+caugello@users.noreply.github.com>
2025-05-12 16:55:01 +0200

    chore(CI): build and prettier step (#23)
    * chore(CI): build and prettier step
    
    Signed-off-by: Christophe Augello <caugello@redhat.com>

--- plugins/quay/src/index.ts
+++ plugins/quay/src/index.ts
@@ -1,9 +1,9 @@
-import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
+import { unstable_ClassNameGenerator as ClassNameGenerator } from "@mui/material/className";
 
-ClassNameGenerator.configure(componentName => {
-  return componentName.startsWith('v5-')
+ClassNameGenerator.configure((componentName) => {
+  return componentName.startsWith("v5-")
     ? componentName
     : `v5-${componentName}`;
 });
 
-export * from '@backstage-community/plugin-quay';
+export * from "@backstage-community/plugin-quay";
